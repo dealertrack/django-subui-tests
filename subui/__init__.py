8e4dd55d3494fb3470bfd4d004c9553be38c8951
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2017-04-25 11:04:13 -0400

    fixed description in __init__

--- subui/__init__.py
+++ subui/__init__.py
@@ -219,4 +219,4 @@ from __future__ import unicode_literals
 
 __author__ = 'Miroslav Shubernetskiy'
 __version__ = '0.1.0'
-__description__ = 'Python dict with stacked context data'
+__description__ = 'Framework to make workflow server integration test suites'
